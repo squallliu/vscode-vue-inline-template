75b31d73862cd3a04485c59d4960d6a9c4e1780c
squall <squall_lau@qq.com>
2021-06-01 17:00:45 +0800

    添加配置

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as vscode from 'vscode';
+
+const typeScriptExtensionId = 'vscode.typescript-language-features';
+const pluginId = 'typescript-lit-html-plugin';
+
+export async function activate(context: vscode.ExtensionContext) {
+  const extension = vscode.extensions.getExtension(typeScriptExtensionId);
+  if (!extension) {
+    return;
+  }
+
+  await extension.activate();
+  if (!extension.exports || !extension.exports.getAPI) {
+    return;
+  }
+  const api = extension.exports.getAPI(0);
+  if (!api) {
+    return;
+  }
+
+  synchronizeConfiguration(api);
+}
+
+function synchronizeConfiguration(api: any) {
+  api.configurePlugin(pluginId, {
+    "tags": [
+    ]
+  });
+}
